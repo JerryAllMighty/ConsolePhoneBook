b988b8be8858ac2bdda305ef57debc5a4d9ed65f
윤민 김 <dbsalszz@naver.com>
2020-09-17 18:21:11 +0900

    초기 커밋.

---
+++ ConsolePhoneBook/PhoneBookManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+    class PhoneBookManager
+    {
+        const int MAX_CNT = 10;
+        PhoneInfo[] infoStorage = new PhoneInfo[MAX_CNT];
+        int curCnt = 0;
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("--------------------주소록--------------------");
+            Console.WriteLine("1.입력 |   2.목록    |   3.검색    |   4.삭제    |   5.종료");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("선택 : ");
+
+        }
+
+        public void InputData()
+        {
+            Console.WriteLine("이름을 입력해주세요");
+            string inputname = Console.ReadLine();      //이 입력값을 배열에 넣으려고 함
+            Console.WriteLine("전화번호를 입력해주세요");
+            string inputphonenumber = Console.ReadLine();
+            Console.WriteLine("생일을 입력해주세요");
+            string inputbirth = Console.ReadLine();
+
+            
+        }
+
+        public void ListData()
+        {
+
+        }
+
+        public void SearchData()
+        {
+            
+        }
+
+        public void DeleteData()
+        {
+
+        }
+
+        
+    }
+}
---
+++ ConsolePhoneBook/PhoneInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhoneBook
+{
+    public class PhoneInfo
+    {
+        string name;        //필수
+        string phonenumber;     //필수
+        string birth;
+
+        public PhoneInfo()
+        {
+
+        }
+
+        public PhoneInfo(string name, string phonenumber)
+        {
+            this.phonenumber = phonenumber;
+            this.name = name;
+        }
+
+        public PhoneInfo(string name, string phonenumber, string birth)
+        {
+            this.phonenumber = phonenumber;
+            this.name = name;
+            this.birth = birth;
+        }
+
+
+
+        public void ShowPhoneInfo()
+        {
+            Console.WriteLine($"이름은 {name}이고, 전화번호는 {phonenumber}이며, 생일은 {birth}입니다.");
+            return;
+        }
+
+        
+    }
+}
